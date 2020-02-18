cfda9cd26fce8834cab14be1935d1879541835a0
astro <out.business.star@gmail.com>
2020-02-19 01:27:30 +1000

    Remove console.log reconnecting...

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export default class Dagger extends EventEmitter {
         }
       })
       this._client.on('reconnect', () => {
-        console.log('reconnecting...')
+        // console.log('reconnecting...')
       })
     })
 
